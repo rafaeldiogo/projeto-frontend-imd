8d597b61b8b5f5b623188cf8a3f03e6d2d859be3
rafaeldiogo <fael.diogo@hotmail.com>
2022-12-29 09:40:06 -0300

    Subindo projeto

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <p>
+        FrontInsta &copy; 2022
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
